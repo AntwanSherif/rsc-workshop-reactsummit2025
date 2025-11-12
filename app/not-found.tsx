5a1abc3595a576280fd6bd19a0ac90d3ee29f7d0
AntwanSherif <antwansherif@gmail.com>
2025-11-12 17:19:16 +0100

    Global 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div className="flex flex-col gap-4">
+      <h1>404 Not Found</h1>
+
+      <p>Sorry, the page you requested is not found</p>
+    </div>
+  );
+}
