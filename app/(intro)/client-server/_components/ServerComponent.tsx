c9750a13a2bcb2f98febc26c1bcf0c2b5b6e3622
AntwanSherif <antwansherif@gmail.com>
2025-11-12 16:49:41 +0100

    Fetch from server component

--- app/(intro)/client-server/_components/ServerComponent.tsx
+++ app/(intro)/client-server/_components/ServerComponent.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
+import { prisma } from '@/db';
 
-export default function ServerComponent() {
-  return <div className="rounded border-2 border-red-500 p-4">ServerComponent</div>;
+export default async function ServerComponent() {
+  const data = await prisma.contact.findMany();
+
+  return (
+    <div className="rounded border-2 border-red-500 p-4">
+      ServerComponent
+      {data[0].first}
+    </div>
+  );
 }
