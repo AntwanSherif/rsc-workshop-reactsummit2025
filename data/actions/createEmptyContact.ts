0bba506f1daad35a6fdde1821f075ea81531d2c4
AntwanSherif <antwansherif@gmail.com>
2025-11-12 18:19:41 +0100

    revalidate after creation

--- data/actions/createEmptyContact.ts
+++ data/actions/createEmptyContact.ts
@@ -1,8 +1,11 @@
 'use server';
+import { revalidatePath } from 'next/cache';
 import { prisma } from '@/db';
 
 export async function createEmptyContact() {
   await prisma.contact.create({
     data: {},
   });
+
+  revalidatePath('/');
 }
